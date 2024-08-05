828483452258f69160b3ee48e59f3ee072bb6d2a
luigischmitt <schmittluigi@gmail.com>
2024-08-04 23:16:36 -0300

    feat: add solutions

--- Solutions/1042.c
+++ Solutions/1042.c
@@ -1,43 +1,31 @@
-// beecrowd | 1042 (Simple Sort) - não finalizado
+// beecrowd | 1042 (Simple Sort)
 
 #include <stdio.h>
-
 int main() {
-
     int a, b, c;
+    int sorted[3];
 
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a == b == c || a > b > c) {
-        printf("%d\n", a);
-        printf("%d\n", b);
-        printf("%d\n", c);
-    } else if (a > b && c > b) {
-        printf("%d\n", a);
-        printf("%d\n", c);
-        printf("%d\n", b);
-    } else if (b > a && a >= c) {
-        printf("%d\n", b);
-        printf("%d\n", a);
-        printf("%d\n", c);
-    } else if (b > c && c >= a) {
-        printf("%d\n", b);
-        printf("%d\n", c);
-        printf("%d\n", a);
-    } else if (c > a && a >= b) {
-        printf("%d\n", c);
-        printf("%d\n", a);
-        printf("%d\n", b);
-    } else {
-        printf("%d\n", c);
-        printf("%d\n", b);
-        printf("%d\n", a);
+    sorted[0] = a;
+    sorted[1] = b;
+    sorted[2] = c;
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2 - i; j++) {
+            if (sorted[j] > sorted[j + 1]) {
+                int temp = sorted[j];
+                sorted[j] = sorted[j + 1];
+                sorted[j + 1] = temp;
+            }
+        }
     }
 
+    printf("%d\n%d\n%d\n", sorted[0], sorted[1], sorted[2]);
+
     printf("\n");
-    printf("%d\n", a);
-    printf("%d\n", b);
-    printf("%d\n", c);  
-    
+
+    printf("%d\n%d\n%d\n", a, b, c);
+
     return 0;
 }
